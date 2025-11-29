916f44534eb8b52b4b835a9e3081551e4746a0ca
leesy010504 <leesy010504@gmail.com>
2025-11-29 19:24:31 +0900

    fix websocket issue3

--- chat-service/src/main/java/com/khi/chatservice/properties/WebSocketProperties.java
+++ chat-service/src/main/java/com/khi/chatservice/properties/WebSocketProperties.java
@@ -4,7 +4,7 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Data
-@ConfigurationProperties(prefix = "jamjam.websocket")
+@ConfigurationProperties(prefix = "tictactalk.websocket")
 public class WebSocketProperties {
     private String endpoint = "/ws-chat";
 }
