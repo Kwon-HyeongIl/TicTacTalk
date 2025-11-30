19605500139c8d985dcd641adcd05df19cfcaa60
kokeunho <kokangsik65@naver.com>
2025-11-30 21:27:39 +0900

    feat: 프로필 이미지 Object Storage에서 삭제 메서드

--- security-service/src/main/java/com/khi/securityservice/core/service/NcpStorageService.java
+++ security-service/src/main/java/com/khi/securityservice/core/service/NcpStorageService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
@@ -26,6 +27,7 @@ public class NcpStorageService {
         this.s3Client = s3Client;
     }
 
+    // 프로필 이미지 업로드
     public String uploadFile(MultipartFile file) {
         try {
             String fileName = file.getOriginalFilename();
@@ -47,5 +49,27 @@ public class NcpStorageService {
         } catch (IOException e) {
             throw new RuntimeException("파일 업로드 실패", e);
         }
+    }
+    // 프로필 이미지 삭제
+    public void deleteFile(String profileImageUrl) {
+
+        String objectKey = extractObjectKey(profileImageUrl);
+
+        DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                .bucket(bucketName)
+                .key(objectKey)
+                .build();
+
+        s3Client.deleteObject(deleteObjectRequest);
+    }
+    // URL에서 삭제할 이미지 key 추출
+    public String extractObjectKey(String profileImageUrl) {
+
+        int bucketIdx = profileImageUrl.indexOf(bucketName);
+        if (bucketIdx == -1) {
+            throw new IllegalArgumentException("Invalid file URL: bucket name not found");
+        }
+
+        return profileImageUrl.substring(bucketIdx + bucketName.length() + 1);
     }
 }
